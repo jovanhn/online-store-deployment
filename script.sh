b41de0fee5fa66a64bb1091756b56f238f7b958a
Jovan Suberic <suberic.jovan@gmail.com>
2022-09-30 19:51:32 +0200

    Initial commit

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+cd ./kube
+# export BRANCH=${CI_COMMIT_REF_NAME}
+# export NAMESPACE=mini-${BRANCH}
+export BRANCH=dev
+export NAMESPACE=mini-${BRANCH}
+
+# kustomization
+sed -i "s~<NAMESPACE>~${NAMESPACE}~g" kustomization.yaml
+sed -i "s~<BRANCH>~${BRANCH}~g" kustomization.yaml  
+
+
+#config
+cd ./config
+sed -i "s~<GITLAB-COMMIT-REF>~\"${CI_COMMIT_SHA}\"~g" annotations.yaml
+sed -i "s~<GITLAB-PIPELINE-REF>~\"${CI_PIPELINE_ID}\"~g" annotations.yaml
+sed -i "s~<REGISTRY_URL>~${CI_REGISTRY_IMAGE}~g" images.yaml
+sed -i "s~<VERSION>~${VERSION}~g" images.yaml
+sed -i "s~<BRANCH>~${BRANCH}~g" config-map-ref.yaml
+sed -i "s~<SUFFIX>~~g" config-map-ref.yaml annotations.yaml images.yaml
+cd ..
+
+
+# resources
+cd ./resources
+# tolerations and node selector
+sed -i "s~<TOLERATIONS_KEY>~\"${TOLERATIONS_KEY}\"~g" *
+sed -i "s~<NODE_AFFINITY>~\"${NODE_AFFINITY}\"~g" *
+
+sed -i "s~<SUFFIX>~~g" bff-store.yaml
+cd ..
+
+kubectl kustomize ./
+kubectl apply -k .
