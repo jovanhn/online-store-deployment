b1a1c427f6f2b9c013ce2508d28abf575f2a284a
Jovan Suberic <suberic.jovan@gmail.com>
2022-10-01 11:59:44 +0200

    create first deployment

---
+++ delete-all-resources.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+kubectl delete svc bff-store-service -n mini-dev
+# service "bff-store-service" deleted
+
+
+
+kubectl delete deployment bff-store -n mini-dev
+# deployment.apps "bff-store" deleted
+
+kubectl delete cm bff-store-properties-dev  -n mini-dev
+# configmap "bff-store-properties-dev" deleted
+
+kubectl delete ingress mini-ingress -n mini-dev
+# ingress.networking.k8s.io "mini-ingress" deleted
